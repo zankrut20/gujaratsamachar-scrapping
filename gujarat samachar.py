eb4067129bc9160227118d3db89cd6586c690ca8
Zankrut Goyani <68992386+zankrut20@users.noreply.github.com>
2023-02-15 17:54:56 +0530

    update the gs.py and add requirement.txt

--- gujarat samachar.py
+++ gujarat samachar.py
@@ -1,4 +1,3 @@
-# required modules
 import requests
 import urllib.request
 import os
@@ -6,43 +5,50 @@ import datetime
 import re
 from bs4 import BeautifulSoup
 from PIL import Image
-from PyPDF2 import  PdfFileReader, PdfFileMerger
+from PyPDF2 import  PdfReader, PdfMerger
 
-# auto generation of website link
+# Creating Folders
+dir_names = ['images/', 'PDFs/']
+for name in dir_names:
+    try:
+        os.mkdir(name)
+    except:
+        FileExistsError
+        print("Foler already exists")
+
+# Scraping Page Links
 html = "https://epaper.gujaratsamachar.com/ahmedabad/" + datetime.datetime.today().strftime('%d-%m-%Y') + "/1"
 r = requests.get(html)
 print("Website is connected")
 soup = BeautifulSoup(r.content, 'html.parser')
-
-# find pages & their respective download links
 pages = soup.find("ul", {"class": "nav nav-tabs nav-dots border-bottom-0"})
 listofPage = []
 for page in pages.find_all("a", {"class":"anchor_click"}, href = True):
     listofPage.append(page['href'])
 
-# issue pages are in .jpef format
-# download all pages in .jpeg format
-# download function
+# Downloading Images
 def download_image(url, file_path, file_name):
     full_path = file_path + file_name + '.jpg'
     urllib.request.urlretrieve(url, full_path)
 
-print("Downloading pages...")
+# Scraping Image Links
 img = []
 for pages in listofPage:
     p = BeautifulSoup(requests.get(pages).content, 'html.parser')
     s = p.find("img", {"class":"w-100 sky epaper_page"})
     img.append(s.get('src'))
+print("Page links were scrapped!")
 
-for url in img:
-    download_image(url, file_path = "images/", file_name = url.split("/")[4].split("-")[3].split(".")[0])
+# Downloading Images
+for i, url in enumerate(img):
+    # construct the file name for the current image
+    file_name = f"{i+1:02d}"
+    # download the image using the current file name
+    download_image(url, file_path="images/", file_name=file_name)
 print("All pages downloaded as images.")
-# convert all pages to pdf
-# current_dir = os.getcwd() # current directory
-image_list = os.listdir("images/") # list of images in images folder
-image_list.sort(key=lambda f: int(re.sub('\D', '', f))) # sort images in ascending order
 
-# convert images to pdf
+# Converting Images to PDF
+image_list = os.listdir("images/") # list of images in images folder
 for image in image_list:
     fimg = Image.open("images/" + image)
     a4img = Image.new("RGB", (2800, 3974), (255, 255, 255))
@@ -50,10 +56,20 @@ for image in image_list:
     a4img.save("PDFs/" + image + ".pdf", "PDF", quality = 70)
 print("All images are converted to PDF format.")
 
-# merge all pdfs into one pdf
+# Merging PDFs
 pdfs = os.listdir("PDFs/")
 pdfs.sort(key=lambda f: int(re.sub('\D', '', f)))
-merger = PdfFileMerger()
+merger = PdfMerger()
 for file in pdfs:
-    merger.append(PdfFileReader("PDFs/" + file, 'rb'))
+    merger.append(PdfReader("PDFs/" + file, 'rb'))
 merger.write("Gujarat Samachar_" + datetime.datetime.today().strftime('%d-%m-%Y') + ".pdf")
+print("All PDFs are merged into one PDF file.")
+
+# Deleting all Images
+for image in image_list:
+    os.remove("images/" + image)
+print("Deleted All Images.")
+# Deleting all Single PDFs
+for pdf in pdfs:
+    os.remove("PDFs/" + pdf)
+print("Deleted All Single PDFs")
